93676120e1ac5198de7431813ff83e2e4df8d2bb
kg-phantom <ksurbayan@gmail.com>
2022-01-19 21:51:50 -0800

    setup server and start typeDefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const { AuthenticationError } = require("apollo-server-express");
+// const { User, Post } = require("../models");
+// const { signToken } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+
+    }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Restroom {
+    restroomId: String
+    name: String
+    street: String
+    city: String
+    directions: String
+    accessible: Boolean
+    unisex: Boolean
+    changingTable: Boolean
+    lat: Float
+    long: Float
+  }
+
+  type Post {
+    _id: ID
+    user: User
+    rating: Int
+    restroom: Restroom
+    postText: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    posts: [Post]
+  }
+
+  type Query {
+      users: [User]
+      user(_id: ID!): User
+      restrooms(lat: Float, long: Float): [Restroom]
+      restroom(restroomId: String!): Restroom
+      posts(userId: ID): [Post]
+      post(_id: ID!): Post
+  }
+`;
+
+module.exports = typeDefs;
