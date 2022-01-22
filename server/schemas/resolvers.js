1de5c36f05df596abd7901bcac6e56cf958fd5c1
kg-phantom <ksurbayan@gmail.com>
2022-01-21 19:29:35 -0800

    addUser and login mutations working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,6 +40,30 @@ const resolvers = {
       return Restroom.findOne({ restroomId });
     },
   },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if(!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if(!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    }
+  }
 };
 
 module.exports = resolvers;
