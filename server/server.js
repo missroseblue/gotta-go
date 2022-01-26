f1ad7bf7af3a9db0a5da91da55865de9c7b6ebf1
kg-phantom <ksurbayan@gmail.com>
2022-01-25 22:37:46 -0800

    cleaned up unecessary lines in server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
@@ -7,7 +6,6 @@ const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require('./utils/auth');
 const db = require("./config/connection");
 
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -32,30 +30,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-
-app.get('/api/search', async (req, res) => {
-  try {
-    const zip = `${req.query.zip}`;
-
-    // use node-fetch to call the open weather api, and reads the key from .env
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${process.env.OPENWEATHER_API_KEY}`,
-    );
-
-    const { coord } = await response.json();
-    console.log(coord);
-
-    return res.json({
-      success: true,
-      coord,
-    });
-  } catch (err) {
-    return res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-});
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
