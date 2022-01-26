4145c6a29dac091d31521ef692b413ccb491e923
Sandra Caceres <missroseblue@gmail.com>
2022-01-25 18:04:26 -0800

    removed duplicated input fields on the forms

--- client/src/components/Forms/LogInForm.js
+++ client/src/components/Forms/LogInForm.js
@@ -7,12 +7,7 @@ function LogInForm() {
             <div className="row">
                 <h1 className="logIn">Log In</h1>
                 <form className="col s12" >
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="password" type="password" className="validate" />
-                            <label htmlFor="password">Password</label>
-                        </div>
-                    </div>
+                    
                     <div className="row">
                         <div className="input-field col s12">
                             <input id="email"  type="email" className="validate" />
@@ -20,17 +15,13 @@ function LogInForm() {
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col s12">
-                            This is an inline input field:
-                            <div className="input-field inline">
-                                <input id="email_inline" type="email" className="validate" />
-                                <label htmlFor="email_inline">Email</label>
-                                <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                            </div>
+                        <div className="input-field col s12">
+                            <input id="password" type="password" className="validate" />
+                            <label htmlFor="password">Password</label>
                         </div>
                     </div>
+                
                     <button className='btn waves-effect waves-light' type='submit' name='action'>Sign In
-                    <i className='material-icons right'>send</i>
                     </button>
                 </form>
             </div>
