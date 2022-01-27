e83e70db1725cdeb2d8ec6110620b6474a62c6c4
kg-phantom <ksurbayan@gmail.com>
2022-01-26 20:00:53 -0800

    save changes to user input to formState

--- client/src/components/Forms/SignUpForm.js
+++ client/src/components/Forms/SignUpForm.js
@@ -1,44 +1,60 @@
-import React from 'react';
-
+import React, { useState } from "react";
 
 function SignUpForm() {
- 
-    // submit(event) {
-    //     event.preventDefault();
-    // }
-        return (
-            <div className="row">
-                <h1 className="signUp">Become A Member</h1>
-                <form className="col s12" >
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <input id="firstName"  type="text" className="validate" />
-                            <label htmlFor="firstName">First Name</label>
-                        </div>
-                        <div className="input-field col s6">
-                            <input id="lastName"  type="text" className="validate" />
-                            <label htmlFor="lastName">Last Name</label>
-                        </div>
-                    </div>
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="email"  type="email" className="validate" />
-                            <label htmlFor="email">Email</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="password" type="password" className="validate" />
-                            <label htmlFor="password">Password</label>
-                        </div>
-                    </div>
-    
-                    <button className='btn waves-effect waves-light' type='submit' name='action'>Submit
-                    </button>
-                </form>
-            </div>
-        );
-    }
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { id } = event.target;
+
+    setFormState({
+        ...formState,
+        [id]: event.target.value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="row">
+      <h1 className="signUp">Become A Member</h1>
+      <form className="col s12" onSubmit={handleFormSubmit}>
+        <div className="row">
+          <div className="input-field col s6">
+            <input id="username" type="text" className="validate" onChange={handleChange} />
+            <label htmlFor="username">Username</label>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="email" type="email" className="validate" onChange={handleChange} />
+            <label htmlFor="email">Email</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="password" type="password" className="validate" onChange={handleChange} />
+            <label htmlFor="password">Password</label>
+          </div>
+        </div>
+
+        <button
+          className="btn waves-effect waves-light"
+          type="submit"
+          name="action"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default SignUpForm;
